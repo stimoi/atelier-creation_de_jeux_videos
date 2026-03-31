ec18117e3aa0024d71d2acde22be9667b8b883ad
GuinnyPig <loic.beylat@gmail.com>
2026-03-31 17:00:22 +0200

    Update createur_de_niveau.py

--- createur_de_niveau.py
+++ createur_de_niveau.py
@@ -33,7 +33,8 @@ BLOCS = {
     "terre": {"texture": 'assets/texture/dirt.png'},
     "pierre": {"texture": 'assets/texture/stone.png'},
     "brique": {"texture": 'assets/texture/dirt.png'},
-    "eau": {"texture": 'assets/texture/dirt.png'},
+    "eau": {"texture": 'assets/texture/water.png'},
+    "lave": {"texture": 'assets/texture/lava.png'},
     "spawn": {"texture": 'assets/texture/spawn.png'},
     "end_level": {"texture": 'assets/texture/end_level.png'}
 }

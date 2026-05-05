6793a6ea7f39e1d023fdaf8ca5ad76f98db6be7e
SaurFort <tou.nathan@gmail.com>
2026-05-05 16:19:41 +0200

    Fix: nom texture brique

--- blocs.py
+++ blocs.py
@@ -2,7 +2,7 @@ BLOCS = {
   "herbe": {"texture": 'assets/texture/grass.png'},
   "terre": {"texture": 'assets/texture/dirt.png'},
   "pierre": {"texture": 'assets/texture/stone.png'},
-  "brique": {"texture": 'assets/texture/brick.png'},
+  "brique": {"texture": 'assets/texture/bricks.png'},
   "eau": {"texture": 'assets/texture/water.png'},
   "lave": {"texture": 'assets/texture/lava.png'},
   "spawn": {"texture": 'assets/texture/spawn.png'},

196ab50a6ff5d537744d80d86c6f1acec5b29ac0
GuinnyPig <loic.beylat@gmail.com>
2026-05-05 15:46:19 +0200

    Update createur_de_niveau.py
    dirt to bricks

--- createur_de_niveau.py
+++ createur_de_niveau.py
@@ -32,7 +32,7 @@ BLOCS = {
     "herbe": {"texture": 'assets/texture/grass.png'},
     "terre": {"texture": 'assets/texture/dirt.png'},
     "pierre": {"texture": 'assets/texture/stone.png'},
-    "brique": {"texture": 'assets/texture/dirt.png'},
+    "brique": {"texture": 'assets/texture/bricks.png'},
     "eau": {"texture": 'assets/texture/water.png'},
     "lave": {"texture": 'assets/texture/lava.png'},
     "spawn": {"texture": 'assets/texture/spawn.png'},
